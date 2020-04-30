acd099910dd0bab826e76d6c8ca485d7f2c9ae15
Andrew P <andrewphaviseth@gmail.com>
2020-04-30 04:15:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,24 +19,66 @@ WIN_COMBINATIONS = [
 ]
 
 
-def won?(board)
-    WIN_COMBINATIONS.each do |win_combination|
+# def won?(board)
+#     WIN_COMBINATIONS.each do |win_combination|
+# 
+#       first_index = win_combination[0]
+#       second_index = win_combination[1]
+#       third_index = win_combination[2]
+# 
+#       position_1 = board[first_index]
+#       position_2 = board[second_index]
+#       position_3 = board[third_index]
+# 
+#       if position_1 == "X" && position_2 == "X" && position_3 == "X"  ||
+#          position_1 == "O" && position_2 == "O" && position_3 == "O"
+#           return win_combination
+#       else
+#           return false
+#       end
+#     end
+# end
+# 
+# def full?(board)
+#   board.none?{|i| i == " "}
+# end
+# 
+# def draw?(board)
+#   if won?(board) == false && full?(board) == true
+#     true
+#   end
+# end
+# 
+# def over?(board)
+#   if won?(board) != false || full?(board) == true || draw?(board) == true
+#     true
+#   end
+# end
+# 
+# def winner(board)
+#   if won?(board) != false
+#     board[won?(board)[0]]
+#   end
+# end
 
-      first_index = win_combination[0]
-      second_index = win_combination[1]
-      third_index = win_combination[2]
+def won?(board)
+  won_check = false
+    WIN_COMBINATIONS.each do |combination|
+      first_index = combination[0]
+      second_index = combination[1]
+      third_index = combination[2]
 
       position_1 = board[first_index]
       position_2 = board[second_index]
       position_3 = board[third_index]
 
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"  ||
-         position_1 == "O" && position_2 == "O" && position_3 == "O"
-          return win_combination
-      else
-          return false
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X" ) ||
+         (position_1 == "O" && position_2 == "O" && position_3 == "O" )
+          return combination
+          won_check = true
       end
     end
+  won_check
 end
 
 def full?(board)
