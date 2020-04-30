9a9c62dfe6990f22c703f2a2fb3aeb983484f78c
Andrew P <andrewphaviseth@gmail.com>
2020-04-30 03:49:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,8 +30,8 @@ def won?(board)
       position_2 = board[second_index]
       position_3 = board[third_index]
 
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X" ) ||
-         (position_1 == "O" && position_2 == "O" && position_3 == "O" )
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"  ||
+         position_1 == "O" && position_2 == "O" && position_3 == "O" 
           return combination
 
       else
