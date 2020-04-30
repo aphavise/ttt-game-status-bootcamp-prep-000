81c251f8c8a87f522e963b25bd3dccae0a680ef2
Andrew P <andrewphaviseth@gmail.com>
2020-04-30 04:01:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ WIN_COMBINATIONS = [
 
 
 def won?(board)
-  won_check = false
+  # won_check = false
     WIN_COMBINATIONS.each do |win_combination|
 
       first_index = win_combination[0]
@@ -39,7 +39,7 @@ def won?(board)
           return false
       end
     end
-  won_check
+  # won_check
 end
 
 def full?(board)
